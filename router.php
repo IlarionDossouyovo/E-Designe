653ed04a859ca6824642b82d3e1236d392b35b9d
ELECTRON <openhands@all-hands.dev>
2026-06-11 13:01:49 +0000

    Debug router to see files

--- router.php
+++ router.php
@@ -1,49 +1,20 @@
 <?php
-// E-Graphisme Router - Simple Version
+// E-Graphisme Router - Debug Version
 $uri = $_SERVER['REQUEST_URI'] ?? '/';
 $path = parse_url($uri, PHP_URL_PATH);
 $path = trim($path, '/');
 
-// Get directory
 $dir = __DIR__;
 
-// Map routes to files
-$routes = [
-    '' => 'index.html',
-    'index' => 'index.html',
-    'portfolio' => 'portfolio.html',
-    'services' => 'services.html',
-    'studio' => 'studio.html',
-    'contact' => 'contact.html',
-    'about' => 'about.html',
-    'blog' => 'blog.html',
-    'privacy' => 'privacy.html',
-    'terms' => 'terms.html',
-];
-
-// Check if route exists in our map
-if (isset($routes[$path])) {
-    $file = $dir . '/' . $routes[$path];
-    if (file_exists($file)) {
-        readfile($file);
-        exit;
-    }
+// Debug: show what we're looking for
+$debug = "Searching for: '$path' in dir: '$dir'<br>";
+$debug .= "Files in dir:<br>";
+$files = scandir($dir);
+foreach ($files as $f) {
+    $debug .= "- $f<br>";
 }
 
-// Try direct file with .html
-$file = $dir . '/' . $path . '.html';
-if (file_exists($file)) {
-    readfile($file);
-    exit;
-}
+echo $debug;
+exit;
 
-// Try without extension
-$file = $dir . '/' . $path;
-if (file_exists($file)) {
-    readfile($file);
-    exit;
-}
-
-// 404
-http_response_code(404);
-echo "404 - Page non trouvée: $path";
+// (rest of code never reached)
